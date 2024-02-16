f33252d9d358183ac79a0fd395655c999f1b96cd
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-02-15 22:00:03 -0300

    15/02/24 jogo de adivinhação
    necessario corrigir problema de limpar campo de texto ao terminar o jogo

--- jogo_adivinhacao/lib/main.dart
+++ jogo_adivinhacao/lib/main.dart
@@ -1,125 +1,134 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(AdivinhacaoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class AdivinhacaoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Adivinhacao(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Adivinhacao extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _AdivinhacaoState createState() => _AdivinhacaoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AdivinhacaoState extends State<Adivinhacao> {
+  final TextEditingController _controllerNumero =
+      TextEditingController(); // Controlador para o campo de entrada de texto
+  String _resultado = ''; // Variável para armazenar o resultado da adivinhação
+  late int
+      _numeroAdivinhar; // Variável para armazenar o número a ser adivinhado
+  bool _adivinhou =
+      false; // Variável para controlar se o usuário já acertou o número ou não
+  int _tentativas = 0; // Variável para contar o número de tentativas
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _iniciarJogo(); // Inicia o jogo quando o widget é inicializado
+  }
+
+  // Função para iniciar um novo jogo
+  void _iniciarJogo() {
+    final random = Random();
+    _numeroAdivinhar =
+        random.nextInt(100) + 1; // Gera um número aleatório entre 1 e 100
+
+    _adivinhou = false; // Reseta o estado de adivinhação
+    _tentativas = 0; // Reseta o número de tentativas
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _resultado = ''; // Reseta o resultado
     });
+  }
+
+  // Função para verificar se o número inserido pelo usuário é correto
+  void _verificarAdivinhacao() {
+    final int? numero = int.tryParse(_controllerNumero.text);
+    //verificação de "numero" digitado pelo usuario para ser inteiro ou nulo
+    //se int o codigo incrementa a tentativa e segue com a logica se não pede para digitar novamente;
+    if (numero != null) {
+      _tentativas++; // Incrementa o número de tentativas
+      //logica para verificar o numero digitado pelo usuario
+      //se "numero" = "_numeroAdivinhar"  logo  "_adivinhou" = "true" , assim encerra a logica;
+      if (numero == _numeroAdivinhar) {
+        setState(() {
+          _adivinhou = true;
+          _resultado =
+              'Parabéns! Você adivinhou o número $_numeroAdivinhar em $_tentativas tentativas!'; // Mostra a mensagem de acerto
+        });
+      } else if (numero < _numeroAdivinhar) {
+        setState(() {
+          _resultado =
+              'Tente um número maior'; // Mostra a mensagem para tentar um número maior
+        });
+      } else {
+        setState(() {
+          _resultado =
+              'Tente um número menor'; // Mostra a mensagem para tentar um número menor
+        });
+      }
+    } else {
+      setState(() {
+        _resultado =
+            'Por favor, insira um número válido'; // Mensagem de erro para entrada inválida
+      });
+    }
+
+    //logica para limpar o o campo de entrada do usuario (labelText)
+    if (!_adivinhou) {
+      _controllerNumero
+          .clear(); // Limpa o campo de entrada se o usuário não tiver adivinhado corretamente
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Jogo de Adivinhação'), //campo do titulo
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor:
+          const Color.fromARGB(255, 154, 201, 224), // Definindo a cor de fundo
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _controllerNumero,
+              keyboardType: TextInputType.number,
+              decoration:
+                  InputDecoration(labelText: 'Insira um número entre 1 e 100'),
             ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _adivinhou ? _iniciarJogo : _verificarAdivinhacao,
+              child: Text(_adivinhou ? 'Iniciar Novamente' : 'Verificar'),
+            ),
+            SizedBox(height: 16.0),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              _resultado,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
             ),
+            SizedBox(height: 8.0),
+            if (_tentativas > 0)
+              Text(
+                'Tentativas: $_tentativas',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0),
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
