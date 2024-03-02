bfca49ce906b42aa66f6e0cb67415d0ff59d83e8
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-03-01 21:59:33 -0300

    010324

---
+++ exemplo_widgets/lib/main.dart
@@ -0,0 +1,10 @@
+// Importa o pacote de material Flutter
+import 'package:flutter/material.dart';
+
+import 'Exercicio1.dart';
+// Função principal que inicializa a aplicação Flutter
+void main() {
+// Inicia a execução da aplicação com o widget MyApp
+runApp(MyApp());
+}
+
