4306e4a99f4effbec45c3216cfaede0c259ba620
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-03-05 22:01:10 -0300

    05/03/24 - lista de exercicios widgets
    realizar primeiro exercicio da primeira lista de exercicios.

---
+++ exercicio1_widgets/lib/exercicio1.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Container principal - Exercicio 1',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Exercicio 1'),
+        ),
+        body: Center(
+          child: Container(
+            width: 400, // largura do Container
+            height: 400, // altura do Container
+            color: Colors.blue, // cor de fundo do Container
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // alinhamento vertical
+              children: <Widget>[
+                Text(
+                  'Widget Filho1', // texto do primeiro widget
+                  style: TextStyle(color: Colors.white, fontSize: 20), // estilo do texto
+                ),
+                SizedBox(height: 20), // espaço entre os widgets
+                Container(
+                  width: 150, // largura do segundo widget
+                  height: 100, // altura do segundo widget
+                  color: Colors.green, // cor de fundo do segundo widget
+                  child: Center(
+                    child: Text(
+                      'Widget Filho2', // texto do segundo widget
+                      style: TextStyle(color: Colors.white, fontSize: 16), // estilo do texto
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20), // espaço entre os widgets
+                Container(
+                  width: 150, // largura do terceiro widget
+                  height: 100, // altura do terceiro widget
+                  decoration: BoxDecoration(
+                    color: Colors.orange, // cor de fundo do terceiro widget
+                    borderRadius: BorderRadius.circular(10), // bordas arredondadas
+                  ),
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Ação ao pressionar o botão
+                      },
+                      child: Text('Widget Filho3'), // Texto do botão
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ exercicio1_widgets/lib/exercio2.dart
@@ -0,0 +1,51 @@
+// Importa o pacote de material Flutter
+import 'package:flutter/material.dart';
+
+// Função principal que inicializa a aplicação Flutter
+void main() {
+// Inicia a execução da aplicação com o widget MyApp
+  runApp(MyApp());
+}
+
+// Definindo a classe MyApp que herda de StatelessWidget
+class MyApp extends StatelessWidget {
+// Método build, obrigatório em StatelessWidget, retorna a árvore de widgets da aplicação
+  @override
+  Widget build(BuildContext context) {
+// Retorna um MaterialApp, que é o widget raiz da aplicação Flutter
+    return MaterialApp(
+// Configura a tela inicial como um Scaffold
+      home: Scaffold(
+// Barra superior da tela (AppBar)
+        appBar: AppBar(
+// Título da barra superior
+          title: Text('Exercicio 2'),
+        ),
+// Corpo da tela, composto por um Column (coluna vertical) de widgets
+        body: Column(
+          children: [
+// Texto simples como primeiro filho da coluna
+            Text('Titulo'),
+// Container contendo um Row (linha horizontal) de dois Text widgets
+
+            Container(
+              child: Row(
+                children: [
+                  SizedBox(width: 50),//espaçamento antes da coluna
+                  Text('Filho Aninhado 1'), 
+                  SizedBox(width: 50), //espaçamento entre as colunas
+                  Text('Filho Aninhado 1'),
+                  SizedBox(width: 50),
+                  Text('Filho Aninhado 2'),
+                ],
+              ),
+            ),
+
+// Texto simples como segundo filho da coluna
+            Text('Segundo Filho'),
+          ],
+        ),
+      ),
+    );
+  }
+}
