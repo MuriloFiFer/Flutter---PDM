ddc689614867c22b1b1288227d2e0f68e5fe60a1
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-04-02 22:04:19 -0300

    02/04/24

---
+++ exemplo_sqllite/lib/Model.dart
@@ -0,0 +1,22 @@
+class ContatoModel {
+
+  //Atributos
+  int id;
+  String nome;
+  String email;
+  String telefone;
+  String endereco;
+
+  //Construtor
+  ContatoModel({
+    required this.id,
+    required this.nome,
+    required this.email,
+    required this.telefone,
+    required this.endereco,
+  });
+
+  //Mapeamento
+  
+
+}
