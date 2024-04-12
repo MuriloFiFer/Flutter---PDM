5cad3d2742c457982b3366968d838a78806e21ed
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-04-11 21:51:49 -0300

    11/04/24

--- sa2_correcao/lib/main.dart
+++ sa2_correcao/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "SA2",
       debugShowCheckedModeBanner: false, //tira faixa de debug(faixa zebrada)
-      home: LoginPage,
+      home: LoginScreen()
       );
   }
 }
