f891347d431e951c2ca1b1d590b02d3566ceaba7
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-04-12 21:13:22 -0300

    12/04/24 revisao SA2

--- sa2_correcao/lib/main.dart
+++ sa2_correcao/lib/main.dart
@@ -1,19 +1,19 @@
-import 'package:flutter/material.dart';
-import 'package:sa2_correcao/ViewLogin.dart';
+import 'package:flutter/material.dart'; // Importa o pacote Flutter para construção de UIs
+import 'package:sa2_correcao/ViewLogin.dart'; // Importa a tela de login do projeto
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); // Inicia a execução do aplicativo
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget { // Define a classe MyApp que estende StatelessWidget
+  const MyApp({super.key}); // Construtor da classe
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "SA2",
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen()
+  Widget build(BuildContext context) { // Método para construir a UI do aplicativo
+    return MaterialApp( // Retorna um MaterialApp, que configura o aplicativo seguindo o Material Design
+      title: "SA2", // Define o título do aplicativo
+      debugShowCheckedModeBanner: false, // Remove a faixa de depuração em modo de desenvolvimento
+      home: LoginScreen() // Define a tela inicial como LoginScreen
     );
   }
 }
